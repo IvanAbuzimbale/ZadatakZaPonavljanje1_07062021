16fdee382dfd61bb2173f44384259e89a7e33cda
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-06-07 08:36:58 +0200

    Add files via upload

---
+++ ZadatakZaPonavljanje_1_07062021/ZadatakZaPonavljanje_1_07062021/KlasaG.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatakZaPonavljanje_1_07062021
+{
+    class KlasaG
+    {
+        static public int Plus10 (int deset)
+        {
+            int Plus1 = deset + 10;
+            return Plus1;
+        }
+
+        static public int f50 (int pedeset)
+        {
+            int f5 = 50;
+            return f5;
+        }
+    }
+}
---
+++ ZadatakZaPonavljanje_1_07062021/ZadatakZaPonavljanje_1_07062021/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatakZaPonavljanje_1_07062021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string broj;
+            Console.Write("Upišite neki broj: ");
+            broj = Console.ReadLine();
+            Console.WriteLine("Traženi broj sa zbrojem deset je: " + KlasaG.Plus10(Convert.ToInt32(broj)));
+            Console.WriteLine("Metoda f50 je jednaka: " + KlasaG.f50(Convert.ToInt32(broj)));
+            Console.ReadKey();
+        }
+    }
+}
